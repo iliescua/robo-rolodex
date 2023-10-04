d2bd69c170f10386a1b09034e30dc40cdf67035b
iliescua <iliescuandrew@gmail,com>
2023-10-04 13:52:02 -0500

    Fixed issue with cards not showing images by removing the loader attribute

--- src/app/components/card/card.component.jsx
+++ src/app/components/card/card.component.jsx
@@ -1,22 +1,16 @@
 import Image from "next/image";
 import "./card.styles.css";
 
-const imageLoader = ({ src }) => {
-  return `https://${src}?size=180x180`;
-};
-
 const Card = ({ robot }) => {
   const { id, name, email } = robot;
 
   return (
     <div className="card-container">
       <Image
-        loader={imageLoader}
-        src={`robohash.org/${id}`}
+        src={`https://robohash.org/${id}?size=180x180`}
         alt="robot_photo"
         width={180}
         height={180}
-        placeholder="empty"
       />
       <div>
         <h2>{name}</h2>
